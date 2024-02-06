de9d648b4716ebd51d3e0986a42457e92dbe63b3
Matías Gonta <maret2023@gmail.com>
2024-02-06 09:06:49 -0300

    Ready to deploy :rocket:

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,1 @@
-// Swap to 'http://localhost:3000' for development and ' https://tiny-target.herokuapp.com' for production
-export const TINY_TARGET_URL = 'http://localhost:3000/';
+export const TINY_TARGET_URL = 'https://tinytarget.vercel.app/';
